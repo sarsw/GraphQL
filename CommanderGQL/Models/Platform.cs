c2b6d4ed5d743ee497deebd34d191b764b675f33
Steve Russell <steve.russell@swintontechnology.com>
2021-12-21 21:21:32 +0000

    Demonstrated how a schema can be documented using HotChocolate's GraphQLDescription

--- CommanderGQL/Models/Platform.cs
+++ CommanderGQL/Models/Platform.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using HotChocolate;
 
 namespace CommanderGQL.Models
 {
+    [GraphQLDescription("Represents any software or service that has commands")]
     public class Platform
     {
         [Key]
         public int Id { get; set; } 
         [Required]
         public string Name { get; set; }
+
+        [GraphQLDescription("Valid license key for the platform")]
         public string LicenseKey { get; set; }
         public ICollection<Command> Commands { get; set; } = new List<Command>();   // a platform can have 0 or more commands
     }
